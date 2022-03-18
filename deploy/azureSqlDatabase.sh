51132394fe78d9627ed42ddafc0a73399aac070a
Rebo Mark Agustines <rebo.agustines@ext.essilor.com>
2022-03-18 21:13:46 +0800

    fix az account

---
+++ deploy/azureSqlDatabase.sh
@@ -0,0 +1,4 @@
+#az accout set $subscription
+
+#az sql db create -g $resourceGroup -s $sqlServerName -n $dbNameETM --service-objective S0
+#az sql db create -g $resourceGroup -s $sqlServerName -n $dbNameEUS --service-objective S0
