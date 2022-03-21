2ac6fbc6aa3260f8b5e5a2029cc60179b5359ad4
Rebo Mark Agustines <97972878+azuregeko@users.noreply.github.com>
2022-03-21 20:25:45 +0800

    Update azureSqlDatabase.sh

--- deploy/azureSqlDatabase.sh
+++ deploy/azureSqlDatabase.sh
@@ -1,4 +1,5 @@
-#az accout set $subscription
+echo "this is a test"
+
 
 #az sql db create -g $resourceGroup -s $sqlServerName -n $dbNameETM --service-objective S0
 #az sql db create -g $resourceGroup -s $sqlServerName -n $dbNameEUS --service-objective S0
